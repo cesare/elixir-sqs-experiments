d4237da8bb2137e96f340597cffcd209de48cca4
SAWADA Tadashi <cesare@mayverse.jp>
2017-01-25 23:13:01 +0900

    Initial commit

---
+++ lib/sqs.ex
@@ -0,0 +1,18 @@
+defmodule Sqs do
+  @moduledoc """
+  Documentation for Sqs.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sqs.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sqs_test.exs
@@ -0,0 +1,8 @@
+defmodule SqsTest do
+  use ExUnit.Case
+  doctest Sqs
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
