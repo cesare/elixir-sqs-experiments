a48627344e750f447076898044cfefc6f7be919a
SAWADA Tadashi <cesare@mayverse.jp>
2017-01-26 23:35:16 +0900

    Add minimum send/receive functions

--- lib/sqs.ex
+++ lib/sqs.ex
@@ -1,18 +1,17 @@
+alias ExAws.SQS
+
 defmodule Sqs do
-  @moduledoc """
-  Documentation for Sqs.
-  """
+  def send(queue_name, message) do
+    SQS.send_message(queue_name, message)
+    |> ExAws.request
+  end
 
-  @doc """
-  Hello world.
+  def receive(queue_name) do
+    SQS.receive_message(queue_name, wait_time_seconds: 20)
+    |> ExAws.request
+  end
 
-  ## Examples
-
-      iex> Sqs.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def clear(queue_name) do
+    SQS.purge_queue(queue_name) |> ExAws.request
   end
 end
